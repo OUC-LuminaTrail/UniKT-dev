ad3b3bdc3c98b658262d8e5b11fb8193d992c2b4
狮子耗耗 <szhhlionhao@qq.com>
2025-11-08 17:36:55 +0800

    将设备参数默认值设置为None
    训练器会自动使用合适的设备进行计算

--- train_gikt.py
+++ train_gikt.py
@@ -56,7 +56,7 @@ def parse_args():
     # 其他参数
     parser.add_argument("--seed", type=int, default=42, help="Random seed")
     parser.add_argument(
-        "--device", type=str, default="cuda", help="Device (cuda or cpu)"
+        "--device", type=str, default=None, help="Device (cuda or cpu)"
     )
 
     return parser.parse_args()
