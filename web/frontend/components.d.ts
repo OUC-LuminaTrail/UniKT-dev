9030391921d7046b9b25bea9c52cdd7402302ede
狮子耗耗 <szhhlionhao@qq.com>
2026-05-05 22:53:15 +0800

    chore(web): update frontend auto-imports and lock file

--- web/frontend/components.d.ts
+++ web/frontend/components.d.ts
@@ -16,6 +16,8 @@ declare module 'vue' {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElDialog: typeof import('element-plus/es')['ElDialog']
     ElDrawer: typeof import('element-plus/es')['ElDrawer']
+    ElForm: typeof import('element-plus/es')['ElForm']
+    ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElInputNumber: typeof import('element-plus/es')['ElInputNumber']
     ElOption: typeof import('element-plus/es')['ElOption']
